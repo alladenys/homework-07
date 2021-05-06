7ba116336b40b5d11dcc20ed9796d0668cae57d5
Alla Denys <alla.denys.it@gmail.com>
2021-05-06 23:20:11 +0300

    add mentors comment task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,8 +2,10 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 nameInput.addEventListener('input', fillInput);
 function fillInput(event) {
-    if(nameInput.value === ''){
-       nameOutput.textContent = 'незнакомец'; 
-    } 
-    nameOutput.textContent = event.currentTarget.value;
+    if(nameInput.value !== ' '){
+    nameOutput.innerText  = event.currentTarget.value;
+    } else {
+        nameOutput.innerText  = 'незнакомец'; 
+        return;
+    }
 }
